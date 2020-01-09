cc4aca2b38a18a5c7a8daf32bf4dc35f8e4b7726
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 17:59:18 +0530

    comments added

--- wpbook_texonomies.php
+++ wpbook_texonomies.php
@@ -1,76 +1,69 @@
 <?php
-/*
-    ===============================
-    Custom Hierarchical Taxonomy Book Category
-    ===============================
-*/
-// create two taxonomies, genres and writers for the post type "book"
+/**
+ * Custom Hierarchical Taxonomy Book Category
+ * create two taxonomies, genres and writers for the post type "book"
+ */
+function WPbook_Custom_taxonomies() 
+{
+    // Add new taxonomy, make it hierarchical (like categories)
+    $labels = array(
+        'name'              => _x('Genres', 'Genres', 'textdomain'),
+        'singular_name'     => _x('Genre', 'Genre', 'textdomain'),
+        'search_items'      => __('Search Genres', 'textdomain'),
+        'all_items'         => __('All Genres', 'textdomain'),
+        'parent_item'       => __('Parent Genre', 'textdomain'),
+        'parent_item_colon' => __('Parent Genre:', 'textdomain'),
+        'edit_item'         => __('Edit Genre', 'textdomain'),
+        'update_item'       => __('Update Genre', 'textdomain'),
+        'add_new_item'      => __('Add New Genre', 'textdomain'),
+        'new_item_name'     => __('New Genre Name', 'textdomain'),
+        'menu_name'         => __('Genre', 'textdomain'),
+    );
 
-function wpbook1_custom_taxonomies() {
-	// Add new taxonomy, make it hierarchical (like categories)
-	$labels = array(
-		'name'              => _x( 'Genres', 'Genres', 'textdomain' ),
-		'singular_name'     => _x( 'Genre', 'Genre', 'textdomain' ),
-		'search_items'      => __( 'Search Genres', 'textdomain' ),
-		'all_items'         => __( 'All Genres', 'textdomain' ),
-		'parent_item'       => __( 'Parent Genre', 'textdomain' ),
-		'parent_item_colon' => __( 'Parent Genre:', 'textdomain' ),
-		'edit_item'         => __( 'Edit Genre', 'textdomain' ),
-		'update_item'       => __( 'Update Genre', 'textdomain' ),
-		'add_new_item'      => __( 'Add New Genre', 'textdomain' ),
-		'new_item_name'     => __( 'New Genre Name', 'textdomain' ),
-		'menu_name'         => __( 'Genre', 'textdomain' ),
-	);
-
-	$args = array(
-		'hierarchical'      => true,
-		'labels'            => $labels,
-		'show_ui'           => true,
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
         'show_admin_column' => true,
         'update_count_callback' => '_update_post_term_count',
-		'query_var'         => true,
-		'rewrite'           => array( 'slug' => 'genre' ),
-	);
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'genre' ),
+    );
+    register_taxonomy('genre', 'book', $args);
 
-    register_taxonomy( 'genre', 'book', $args );
+    /**
+     * Custom Non Hierarchical Taxonomy Book Category
+     * Add new taxonomy, NOT hierarchical (like tags)
+     */    
+    $labels = array(
+        'name'                       => _x('Writers', 'taxonomy general name', 'textdomain'),
+        'singular_name'              => _x('Writer', 'taxonomy singular name', 'textdomain'),
+        'search_items'               => __('Search Writers', 'textdomain'),
+        'popular_items'              => __('Popular Writers', 'textdomain'),
+        'all_items'                  => __('All Writers', 'textdomain'),
+        'parent_item'                => null,
+        'parent_item_colon'          => null,
+        'edit_item'                  => __('Edit Writer', 'textdomain'),
+        'update_item'                => __('Update Writer', 'textdomain'),
+        'add_new_item'               => __('Add New Writer', 'textdomain'),
+        'new_item_name'              => __('New Writer Name', 'textdomain'),
+        'separate_items_with_commas' => __('Separate writers with commas', 'textdomain'),
+        'add_or_remove_items'        => __('Add or remove writers', 'textdomain'),
+        'choose_from_most_used'      => __('Choose from the most used writers', 'textdomain'),
+        'not_found'                  => __('No writers found.', 'textdomain'),
+        'menu_name'                  => __('Writers', 'textdomain'),
+    );
 
-/*
-    ===============================
-    Custom Non Hierarchical Taxonomy Book Category
-    ===============================
-*/
+    $args = array(
+        'hierarchical'          => false,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_admin_column'     => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var'             => true,
+        'rewrite'               => array('slug' => 'writer'),
+    );
 
-
-	// Add new taxonomy, NOT hierarchical (like tags)
-	$labels = array(
-		'name'                       => _x( 'Writers', 'taxonomy general name', 'textdomain' ),
-		'singular_name'              => _x( 'Writer', 'taxonomy singular name', 'textdomain' ),
-		'search_items'               => __( 'Search Writers', 'textdomain' ),
-		'popular_items'              => __( 'Popular Writers', 'textdomain' ),
-		'all_items'                  => __( 'All Writers', 'textdomain' ),
-		'parent_item'                => null,
-		'parent_item_colon'          => null,
-		'edit_item'                  => __( 'Edit Writer', 'textdomain' ),
-		'update_item'                => __( 'Update Writer', 'textdomain' ),
-		'add_new_item'               => __( 'Add New Writer', 'textdomain' ),
-		'new_item_name'              => __( 'New Writer Name', 'textdomain' ),
-		'separate_items_with_commas' => __( 'Separate writers with commas', 'textdomain' ),
-		'add_or_remove_items'        => __( 'Add or remove writers', 'textdomain' ),
-		'choose_from_most_used'      => __( 'Choose from the most used writers', 'textdomain' ),
-		'not_found'                  => __( 'No writers found.', 'textdomain' ),
-		'menu_name'                  => __( 'Writers', 'textdomain' ),
-	);
-
-	$args = array(
-		'hierarchical'          => false,
-		'labels'                => $labels,
-		'show_ui'               => true,
-		'show_admin_column'     => true,
-		'update_count_callback' => '_update_post_term_count',
-		'query_var'             => true,
-		'rewrite'               => array( 'slug' => 'writer' ),
-	);
-
-	register_taxonomy( 'writer', 'book', $args );
+    register_taxonomy('writer', 'book', $args);
 }
-add_action( 'init', 'wpbook1_custom_taxonomies' );
+add_action('init', 'WPbook_Custom_taxonomies');
