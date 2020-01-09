3239ecf4fa797071e1ce8a59269e543f56ff50ab
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 17:50:32 +0530

    comments added

--- sort-code.php
+++ sort-code.php
@@ -1,13 +1,10 @@
 <?php
-/*
-    ===============================
-    create shortcode
-    ===============================
-*/    
-add_shortcode('book', 'wpbook1_shortcode');
-function wpbook1_shortcode($attributes){
-	//print_r($attributes);	
-    echo "<br/><h5> ID: " . $attributes['id'] . "</h5>";	
+/**
+ * Create shortcode
+ */
+function WPbook_shortcode($attributes) 
+{
+    echo "<br/><h5> ID: " . $attributes['id'] . "</h5>";
     echo "<br/><h5> Author name: " . $attributes['author_name'] . "</h5>";
     echo "<br/><h5> Year: " . $attributes['year'] . "</h5>";
     echo "<br/><h5> Category: " . $attributes['category'] . "</h5>";
@@ -15,3 +12,4 @@ function wpbook1_shortcode($attributes){
     echo "<br/><h5> Publisher:" . $attributes['publisher'] . "</h5><br/><br/>";
     //echo "<br/><h5> price: </h5>" . $attributes['price'] . "<br/><br/>";
 }
+add_shortcode('book', 'WPbook_shortcode');
