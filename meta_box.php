42a1d55f02dd4f2004e01e2cec55709796a72a69
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 17:46:20 +0530

    comments added

--- meta_box.php
+++ meta_box.php
@@ -1,199 +1,201 @@
 <?php
-/*
-    ===============================
-    Custom Meta Boxes
-    ===============================
-*/
-
-// function wpbook1_register_meta_boxes() {
-//     add_meta_box( 'wpbook1-page-id', 'Meta Post', 'wpbook1_pages_function', 'post', 'Normal', 'high');
-// }
-// add_action('add_meta_boxes', 'wpbook1_register_meta_boxes');
-
-function wpbook1_add_custom_box()
+/**
+ * Custom Meta Boxes
+ */
+function WPbook_Add_Custom_box()
 {
-	$screens = array('book');
-	foreach ($screens as $screen) {
+    $screens = array('book');
+    foreach ($screens as $screen) {
         add_meta_box(
-            'wpbook1_box_id',           // Unique ID
-            __( 'Custom Meta Box', 'sitepoint' ),  	// Box title
-			'wpbook1_custom_box_html1', // Content callback, must be of type callable
-			$screen	     // Post type
-		);
-	}
+            'WPbook_box_id',           // Unique ID
+            __('Custom Meta Box', 'sitepoint'),  // Box title
+            'WPbook_Custom_Box_html1', // Content callback, must be of type callable
+            $screen         // Post type
+        );
+    }
 }
-add_action('add_meta_boxes_book', 'wpbook1_add_custom_box');
+add_action('add_meta_boxes_book', 'WPbook_Add_Custom_box');
 
 
-//callback function (metabox) for custom-post-type 'book'
-function wpbook1_custom_box_html1($post)
+/**
+ * Callback function (metabox) for custom-post-type 'book'
+ */
+
+function WPbook_Custom_Box_html1($post)
 {
-	wp_nonce_field(basename(__FILE__), "wpbook1_cpt_nonce");
+    wp_nonce_field(basename(__FILE__), "WPbook_cpt_nonce");
     //$value = get_post_meta($post->ID, '_wpbook1_meta_key', true);
     ?>
-  	<form>
-    	<label for = "textAuthorName">Author Name :</label>
-		<?php $author_name = get_post_meta( $post->ID, 'book_author_name', true ) ?>
-		<input type = "text" name = "textAuthorName" value = "<?php echo $author_name ?>" placeholder = "write author name">
-		</br></br>
-		
-		<label for = "textPublisherName">Publisher Name :</label>
-		<?php $publisher_name = get_post_meta( $post->ID, 'book_publisher_name', true ) ?>
-		<input type = "text" name = "textPublisherName" value = "<?php echo $publisher_name ?>" placeholder = "write publisher name">
-		</br></br>
-		
-		<label for = "textEdition">Edition :</label>
-		<?php $edition_name = get_post_meta( $post->ID, 'book_editor_name', true ) ?>
-		<input type = "text" name = "textEdition" value = "<?php echo $edition_name ?>" placeholder = "Edition">
-		</br></br>
-		
-		<label for = "date">Date :</label>
-		<?php $date = get_post_meta( $post->ID, 'book_date', true ) ?>
-		<input type = "date" name = "date" value = "<?php echo $date ?>" placeholder = "Date">
-		</br></br>
-		
-		<label for = "URL">URL :</label>
-		<?php $url = get_post_meta( $post->ID, 'book_url', true ) ?>
-		<input type = "url" name = "URL" value = "<?php echo $url ?>" placeholder = "URL">
-		</br></br>
-		
-		<label for = "price">Price :</label>
-		<?php $price = get_post_meta( $post->ID, 'book_price', true ) ?>
-		<input type = "text" name = "price" value= "<?php echo $price ?>" placeholder = "Enter price in <?php echo $_SESSION['currency']; ?>">
+    <form>
+        <label for = "textAuthorName">Author Name :</label>
+        <?php $author_name = get_post_meta($post->ID, 'book_author_name', true) ?>
+        <input type = "text" name = "textAuthorName" value = "<?php echo $author_name ?>" placeholder = "write author name">
+        </br></br>
 
-	</form>
-	<?php
+        <label for = "textPublisherName">Publisher Name :</label>
+        <?php $publisher_name = get_post_meta($post->ID, 'book_publisher_name', true) ?>
+        <input type = "text" name = "textPublisherName" value = "<?php echo $publisher_name ?>" placeholder = "write publisher name">
+        </br></br>
+
+        <label for = "textEdition">Edition :</label>
+        <?php $edition_name = get_post_meta($post->ID, 'book_editor_name', true) ?>
+        <input type = "text" name = "textEdition" value = "<?php echo $edition_name ?>" placeholder = "Edition">
+        </br></br>
+
+        <label for = "date">Date :</label>
+        <?php $date = get_post_meta($post->ID, 'book_date', true) ?>
+        <input type = "date" name = "date" value = "<?php echo $date ?>" placeholder = "Date">
+        </br></br>
+
+        <label for = "URL">URL :</label>
+        <?php $url = get_post_meta($post->ID, 'book_url', true) ?>
+        <input type = "url" name = "URL" value = "<?php echo $url ?>" placeholder = "URL">
+        </br></br>
+
+        <label for = "price">Price :</label>
+        <?php $price = get_post_meta($post->ID, 'book_price', true) ?>
+        <input type = "text" name = "price" value= "<?php echo $price ?>" placeholder = "Enter price in <?php echo $_SESSION['currency']; ?>">
+
+    </form>
+    <?php
 }
 
-
-function wpbook1_save_bookdata($post_id, $post)
+/**
+ * Save the metabox field data
+ */
+function WPbook_Save_bookdata($post_id, $post)
 {
-	//veified nonce
-	if( !isset($_POST['wpbook1_cpt_nonce']) || !wp_verify_nonce($_POST['wpbook1_cpt_nonce'], basename(__FILE__)) ){
-		return $post_id;
-	}
+    //veified nonce
+    if (!isset($_POST['WPbook_cpt_nonce']) || !wp_verify_nonce($_POST['WPbook_cpt_nonce'], basename(__FILE__))) {
+        return $post_id;
+    }
 
-	//verifying slug value
-	$post_slug = 'book';
-	if( $post_slug != $post->post_type ) {
-		return;
-	}
+    //verifying slug value
+    $post_slug = 'book';
+    if ($post_slug != $post->post_type) {
+        return;
+    }
 
-	//save publisher value to database field
-	$pub_name = '';
-	if ( isset( $_POST['textPublisherName'] )){
-		$pub_name = sanitize_text_field( $_POST['textPublisherName'] );
-	}
+    //save publisher value to database field
+    $pub_name = '';
+    if (isset($_POST['textPublisherName'])) {
+        $pub_name = sanitize_text_field($_POST['textPublisherName']);
+    }
 
-	//save author value to database field
-	$auth_name = '';
-	if ( isset( $_POST['textAuthorName'] )){
-		$auth_name = sanitize_text_field( $_POST['textAuthorName'] );
-	}
+    //save author value to database field
+    $auth_name = '';
+    if (isset($_POST['textAuthorName'])) {
+        $auth_name = sanitize_text_field($_POST['textAuthorName']);
+    }
 
-	//save edition value to database field
-	$editi = '';
-	if ( isset( $_POST['textEdition'] )){
-		$editi = sanitize_text_field( $_POST['textEdition'] );
-	}
-	//save date & time value to database field
-	$date_time = '';
-	if ( isset( $_POST['date'] )){
-		$date_time = sanitize_text_field( $_POST['date'] );
-	}
-	
-	//save url value to database field
-	$URL = '';
-	if ( isset( $_POST['URL'] )){
-		$URL = sanitize_text_field( $_POST['URL'] );
-	}
+    //save edition value to database field
+    $editi = '';
+    if (isset($_POST['textEdition'])) {
+        $editi = sanitize_text_field($_POST['textEdition']);
+    }
+    //save date & time value to database field
+    $date_time = '';
+    if (isset($_POST['date'])) {
+        $date_time = sanitize_text_field($_POST['date']);
+    }
 
-	//save price value to database field
-	$price_1 = '';
-	if ( isset( $_POST['price'] )){
-		$price_1 = sanitize_text_field( $_POST['price'] );
-	}
+    //save url value to database field
+    $URL = '';
+    if (isset($_POST['URL'])) {
+        $URL = sanitize_text_field($_POST['URL']);
+    }
 
-	update_post_meta($post_id,'book_publisher_name',$pub_name);
-	update_post_meta($post_id,'book_author_name',$auth_name);
-	update_post_meta($post_id,'book_editor_name',$editi);
-	update_post_meta($post_id,'book_url',$URL);
-	update_post_meta($post_id,'book_date',$date_time);
-	update_post_meta($post_id,'book_price',$price_1);
+    //save price value to database field
+    $price_1 = '';
+    if (isset($_POST['price'])) {
+        $price_1 = sanitize_text_field($_POST['price']);
+    }
+
+    update_post_meta($post_id, 'book_publisher_name', $pub_name);
+    update_post_meta($post_id, 'book_author_name', $auth_name);
+    update_post_meta($post_id, 'book_editor_name', $editi);
+    update_post_meta($post_id, 'book_url', $URL);
+    update_post_meta($post_id, 'book_date', $date_time);
+    update_post_meta($post_id, 'book_price', $price_1);
 }
-add_action('save_post', 'wpbook1_save_bookdata', 10, 2);
+add_action('save_post', 'WPbook_Save_bookdata', 10, 2);
 
 
-
-//author metabox
-function wpbook1_author_custom_box()
+/**
+ * Author metabox
+ */
+function WPbook_Author_Custom_box()
 {
-	$screens = array('book');
-	foreach ($screens as $screen) {
+    $screens = array('book');
+    foreach ($screens as $screen) {
         add_meta_box(
-            'wpbook1_author_id',           // Unique ID
-            __( 'author Meta Box', 'sitepoint' ),  	// Box title
-			'wpbook1_author_box_html', // Content callback, must be of type callable
-			$screen	     // Post type
-		);
-	}
+            'WPbook_Author_id',           // Unique ID
+            __('author Meta Box', 'sitepoint'),    // Box title
+            'WPbook_Author_Box_html', // Content callback, must be of type callable
+            $screen        // Post type
+        );
+    }
 }
-add_action('add_meta_boxes_book', 'wpbook1_author_custom_box');
+add_action('add_meta_boxes_book', 'WPbook_Author_Custom_box');
 
-
-function wpbook1_author_box_html($post)
+/**
+ * Author meta box
+ */
+function WPbook_Author_Box_html($post)
 {
-	wp_nonce_field(basename(__FILE__), "wpbook1_author_nonce");
+    wp_nonce_field(basename(__FILE__), "WPbook_Author_nonce");
     ?>
-	<label for="dropDownAuthor">Author Name</label> 
-	<select name="dropDownAuthor" id="dropDownAuthor">
+    <label for="dropDownAuthor">Author Name</label> 
+    <select name="dropDownAuthor" id="dropDownAuthor">
 
-	<?php
-		$post_id = $post->ID;
-		$author_id = get_post_meta($post_id, 'book_author_name', true);
+    <?php
+        $post_id = $post->ID;
+        $author_id = get_post_meta($post_id, 'book_author_name', true);
 
-		$all_authors = get_users(array('role' => 'author'));
-		foreach($all_authors as $index => $author){
-			$selected = "";
-			if ( $author_id == $author->data->ID ){
-				$selected = 'selected="selected"';
-			}
-			?>
-				<option value = "<?php echo $author->data->display_name; ?>" <?php echo $selected ?>;  ><?php echo $author->data->display_name ?></option>
-			<?php
-		}
-	?>
-
-     </select>
-   <?php
+        $all_authors = get_users(array('role' => 'author'));
+        foreach ($all_authors as $index => $author) {
+            $selected = "";
+        if ($author_id == $author->data->ID) {
+            $selected = 'selected="selected"';
+        }
+        ?>
+            <option value = "<?php echo $author->data->display_name; ?>" <?php echo $selected ?>; >
+                <?php echo $author->data->display_name ?>
+            </option>
+        <?php } ?>
+    </select>
+    <?php
 }
 
-function wpbook1_save_author($post_id, $post)
+/**
+ * Save author data
+ */
+function WPbook_Save_author($post_id, $post)
 {
-	//veified nonce
-	if( !isset($_POST['wpbook1_author_nonce']) || !wp_verify_nonce($_POST['wpbook1_author_nonce'], basename(__FILE__)) ){
-		return $post_id;
-	}
+    //veified nonce
+    if (!isset($_POST['WPbook_author_nonce']) || !wp_verify_nonce($_POST['WPbook_author_nonce'], basename(__FILE__))) {
+        return $post_id;
+    }
 
-	//verifying slug value
-	$post_slug = 'book';
-	if( $post_slug != $post->post_type ) {
-		return;
-	}
+    //verifying slug value
+    $post_slug = 'book';
+    if ($post_slug != $post->post_type) {
+        return;
+    }
 
-	//save value to database field
-	$author_name = '';
-	if ( isset( $_POST['dropDownAuthor'] )){
-		$author_name = sanitize_text_field( $_POST['dropDownAuthor'] );
-	} else{
-		$author_name = '';
-	}
+    //save value to database field
+    $author_name = '';
+    if (isset($_POST['dropDownAuthor'])) {
+        $author_name = sanitize_text_field($_POST['dropDownAuthor']);
+    } else {
+        $author_name = '';
+    }
 
-	update_post_meta(
-		$post_id,
-		'book_author_name',
-		$author_name
-	);
+    update_post_meta(
+        $post_id,
+        'book_author_name',
+        $author_name
+    );
 }
-add_action('save_post', 'wpbook1_save_author', 10, 2);
+add_action('save_post', 'WPbook_Save_author', 10, 2);
 
